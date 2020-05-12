768ed28ee77b678beae3f29131ec4fef00d8172e
Jan <janekk3@op.pl>
2020-05-13 00:06:56 +0200

    Inicjalizacja pustych klas wraz z uwzględnieniem implementtacji interfejsu i dziedziczenia

---
+++ Java_projekt/src/main/java/grupa_x/java_projekt/ElementPietra.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupa_x.java_projekt;
+
+/**
+ *
+ * @author Jan
+ */
+public abstract class ElementPietra implements Powierzchnia{  
+
+    @Override
+    public int policzPole() {
+        return 0;
+    }
+    
+}
---
+++ Java_projekt/src/main/java/grupa_x/java_projekt/Garaz.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupa_x.java_projekt;
+
+/**
+ *
+ * @author Jan
+ */
+public class Garaz extends Pomieszczenia{
+    
+}
