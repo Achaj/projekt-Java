1c41eb0bdc1d6e11044f0bc3de5dfc54cd55fe46
Jan <janekk3@op.pl>
2020-05-14 14:27:53 +0200

    tworzenie main

---
+++ Java_projekt/src/main/java/grupa_x/java_projekt/main.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupa_x.java_projekt;
+
+/**
+ *
+ * @author Jan
+ */
+public class main {
+     public static void main(String[] args) {
+        Pomieszczenie[] mojePomieszczenia = {
+            new Pomieszczenie() {
+                {
+                    szerokosc = 3;
+                    dlugosc = 2;
+                    nazwa = "pokoj";
+                }
+            },
+            new Pomieszczenie() {
+                {
+                    szerokosc = 2;
+                    dlugosc = 2;
+                    nazwa = "lazienka";
+                }
+            }
+        };
+
+        Mieszkanie mojeMieszkanie = new Mieszkanie() {
+            {
+                pomieszczenia = mojePomieszczenia;
+                wizytowka = "Kowalscy";
+
+            }
+        };
+        ElementPietra[] mojeElementyParteru = {
+            mojeMieszkanie, new Pomieszczenie() {
+                {
+                    szerokosc = 2;
+                    dlugosc = 2;
+                    nazwa = "suszarnia";
+                }
+            }
+        };
+        Pietro[] mojePietra = {
+            new Pietro() {
+                {
+                    elementy = mojeElementyParteru;
+                    numerPietra = 0;
+
+                }
+            }
+        };
+        Garaz[] mojeGaraze = {
+            new Garaz() {
+                {
+                    szerokoscBramy = 3;
+                    dlugoscBramy = 2;
+                    szerokosc = 5;
+                    dlugosc = 5;
+                }
+            }
+        };
+        Budynek mojBudynek = new Budynek() {
+            {
+                powierzchniaDzialki = 20;
+                pietra = mojePietra;
+                garaze = mojeGaraze;
+            }
+        };
+        System.out.println(mojBudynek.policzPole());
+    }
+}
